4059d728a7d0a8799bb92f09589d1b1dfc36d812
Osmani Hernandez <ohernandez@izotope.com>
2020-03-31 20:16:34 -0400

    add left and right borders. add tools to bottom

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
       
       { /* until we have the drawgrid use div main-grid*/ }
       <div className="main-grid">
+        <div className="left-side-bar"></div>
         <canvas id="canvas"></canvas>
+        <div className="right-side-bar"></div>
       </div>
       
       <ToolPanel />
